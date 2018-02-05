2933e8de144faf5ed0e570363ae0dcffbe968e7c
zeteref <zeteref@gmail.com>
2018-02-05 12:02:30 +0100

    init

---
+++ wykop.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         wykop cleaner
+// @namespace    https://github.com/zeteref/userscripts
+// @version      0.1
+// @description  try to take over the world!
+// @author       Me
+// @match        https://www.wykop.pl*
+// @grant        none
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+	$(document).ready(function() {
+        alert('a');
+	});
+
+    // Your code here...
+})();
