b092da42450d9a42ea5b24280d3a9e320f037d30
zeteref <zeteref@gmail.com>
2018-02-05 12:05:17 +0100

    version up

--- wykop.js
+++ wykop.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         wykop cleaner
 // @namespace    https://github.com/zeteref/userscripts
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       Me
 // @match        https://www.wykop.pl*
