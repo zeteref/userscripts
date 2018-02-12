f7a1c5e4afa543de808037f85f8afbc785e8c5a1
zeteref <zeteref@gmail.com>
2018-02-12 13:11:02 +0100

    replace youtube player

--- wykop.js
+++ wykop.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         wykop cleaner
 // @namespace    https://github.com/zeteref/userscripts
-// @version      0.3.1
+// @version      0.3.3
 // @description  try to take over the world!
 // @author       Me
 // @include *://*wykop.pl*
@@ -12,13 +12,17 @@
 (function() {
     'use strict';
 
-	$(document).ready(function() {
+    $(document).ready(function() {
+        var src = $('.screen > div').attr('id').replace('yt-','');
+        var yt = '<iframe width="100%" height="648" src="https://www.youtube.com/embed/'+src+'" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>';
+        
         $('.screening-displace').remove();
         $('#animation_container').remove();
         $('#fb-root').remove();
         $('[href*="adverts"]').remove();
-	});
+        $('.screen').replaceWith(yt);
+
+    });
 
     // Your code here...
 })();
-
